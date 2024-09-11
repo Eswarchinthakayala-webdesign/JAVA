86b97680dbd8e18597230ad4fcf0815a0db59627
Ch.Eswar <150889483+Eswarchinthakayala-webdesign@users.noreply.github.com>
2024-09-11 21:04:52 +0530

    Create program.java

---
+++ Basics/Input from user/program.java
@@ -0,0 +1,11 @@
+package com.Inputs;
+import java.util.Scanner;
+public class Inputs {
+    public static void main(String[] args) {
+       Scanner input=new Scanner(System.in);
+        System.out.print("Enter the Roll Number: ");
+       int rollNo=input.nextInt();
+        System.out.println("Roll Number is:"+rollNo);
+
+    }
+}
