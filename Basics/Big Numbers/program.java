ac8e387b1e405b1996c6eead60357f4d1d9e26f7
Ch.Eswar <150889483+Eswarchinthakayala-webdesign@users.noreply.github.com>
2024-09-11 21:05:58 +0530

    Create program.java

---
+++ Basics/Big Numbers/program.java
@@ -0,0 +1,9 @@
+package com.Inputs;
+import java.util.Scanner;
+public class Inputs {
+    public static void main(String[] args) {
+       int a=230_000_000;
+        System.out.println(a);
+
+    }
+}
