2027bfbf5bece79c14a271f972423129201fe108
Ch.Eswar <150889483+Eswarchinthakayala-webdesign@users.noreply.github.com>
2024-09-12 22:53:22 +0530

    Create program.java

---
+++ Assignments/Assignment-1/Greetings/program.java
@@ -0,0 +1,12 @@
+package com.Eswar;
+
+import java.util.Scanner;
+
+public class Assignment {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.print("Enter the Name to Send Greetings:");
+        String name=input.next();
+        System.out.println("Hello "+name);
+    }
+}
