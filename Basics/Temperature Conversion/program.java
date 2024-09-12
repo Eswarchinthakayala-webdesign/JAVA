5e69233665816f7cbacaae6c6626391e6b91ec07
Ch.Eswar <150889483+Eswarchinthakayala-webdesign@users.noreply.github.com>
2024-09-12 22:47:32 +0530

    Create program.java

---
+++ Basics/Temperature Conversion/program.java
@@ -0,0 +1,13 @@
+package com.Eswar;
+
+import java.util.Scanner;
+
+public class Temperature {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.print("Enter the Temp in C:");
+        float tempC=input.nextFloat();
+        float tempF=(tempC*9/5)+32;
+        System.out.println("Temperature in F:"+tempF);
+    }
+}
