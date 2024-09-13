7cbfb95aa57c9387bc5ee941f62b0d81b2efc603
Ch.Eswar <150889483+Eswarchinthakayala-webdesign@users.noreply.github.com>
2024-09-13 17:51:24 +0530

    Create program.java

---
+++ Basics/For Loop/program.java
@@ -0,0 +1,14 @@
+package com.Code;
+
+import java.util.Scanner;
+
+public class Code {
+    public static void main(String[] args) {
+       Scanner input=new Scanner(System.in);
+       int num=10;
+       for(int i=1; i<=num;i++)
+       {
+           System.out.println(i);
+       }
+    }
+}
