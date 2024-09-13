134135abe592be2914c25f29afd58379d8538e70
Ch.Eswar <150889483+Eswarchinthakayala-webdesign@users.noreply.github.com>
2024-09-13 18:13:46 +0530

    Create program.java

---
+++ Basics/Do-While Loop/program.java
@@ -0,0 +1,18 @@
+package com.Code;
+
+import java.util.Scanner;
+
+public class Code {
+    public static void main(String[] args) {
+       Scanner input=new Scanner(System.in);
+       int n=1;
+      do {
+
+          System.out.println("Hello World");
+       
+      }
+      while(n!=1);
+
+
+    }
+}
