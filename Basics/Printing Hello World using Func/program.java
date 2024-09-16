c89ab226e9fa7b01dbf29616eacedc04574b482a
Ch.Eswar <150889483+Eswarchinthakayala-webdesign@users.noreply.github.com>
2024-09-16 17:58:48 +0530

    Create program.java

---
+++ Basics/Printing Hello World using Func/program.java
@@ -0,0 +1,13 @@
+package com.Eswar;
+
+import java.util.Scanner;
+
+public class Functions {
+    public static void main(String[] args) {
+       greeting();
+    }
+   static void greeting()
+   {
+       System.out.println("Hello World");
+   }
+}
