8a7c87d42d3691aca22169655e2fe1bad7c9f156
Ch.Eswar <150889483+Eswarchinthakayala-webdesign@users.noreply.github.com>
2024-09-16 18:08:40 +0530

    Create program.java

---
+++ Basics/Returning String/program.java
@@ -0,0 +1,15 @@
+package com.Eswar;
+
+import java.util.Scanner;
+
+public class Functions {
+    public static void main(String[] args) {
+       String message=greeting();
+        System.out.println(message);
+    }
+   static String greeting()
+   {
+      String  greeting="How Are You? ";
+      return greeting;
+   }
+}
