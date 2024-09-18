6ed13bb00de22cd58c525a4f499a3dbf3d853844
Ch.Eswar <150889483+Eswarchinthakayala-webdesign@users.noreply.github.com>
2024-09-18 17:22:52 +0530

    Create program.java

---
+++ Basics/Function Overloading/program.java
@@ -0,0 +1,23 @@
+package com.Eswar;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Functions {
+
+    public static void main(String[] args) {
+        func(10);
+    }
+    static void func(double b)
+    {
+        System.out.println("second one");
+        System.out.println(b);
+    }
+    static void func(int a)
+    {
+        System.out.println("First one ");
+        System.out.println(a);
+    }
+
+
+}
