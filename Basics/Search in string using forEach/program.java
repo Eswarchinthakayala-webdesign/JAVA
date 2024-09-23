f8081f9ca1fe63d65bd20365bbe5cc0c143c845c
Ch.Eswar <150889483+Eswarchinthakayala-webdesign@users.noreply.github.com>
2024-09-23 22:52:42 +0530

    Create program.java

---
+++ Basics/Search in string using forEach/program.java
@@ -0,0 +1,43 @@
+package com.Eswar;
+
+public class Searching {
+    public static void main(String[] args) {
+        String name="Eswar";
+        char target='a';
+        System.out.println(search(name,target));
+        System.out.println(search2(name,target));
+    }
+
+  static boolean search2(String name, char target) {
+        if(name.length()==0)
+        {
+            return false;
+        }
+        for(char ch: name.toCharArray())
+        {
+                if(ch==target)
+                {
+                    return true;
+                }
+        }
+        return false;
+    }
+
+    static boolean search(String str,char target)
+
+    {
+        if(str.length()==0)
+        {
+            return false;
+        }
+        for (int i = 0; i < str.length(); i++) {
+            if(target==str.charAt(i))
+            {
+                return true;
+            }
+
+        }
+        return false;
+
+    }
+}
