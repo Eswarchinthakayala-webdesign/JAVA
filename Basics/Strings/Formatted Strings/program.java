69df6b8754f92e0715147c0db90d39c1e0474c1c
Ch.Eswar <150889483+Eswarchinthakayala-webdesign@users.noreply.github.com>
2024-12-31 14:46:32 +0530

    Create program.java

---
+++ Basics/Strings/Formatted Strings/program.java
@@ -0,0 +1,13 @@
+package com.Eswar;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Practise {
+    public static void main(String[] args) {
+        float num=1234.345432f;
+        System.out.printf("Formatted String %.2f",num);
+
+    }
+
+}
