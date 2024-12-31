2e05498cce72eabbbd9072b33d2540546520ca86
Ch.Eswar <150889483+Eswarchinthakayala-webdesign@users.noreply.github.com>
2024-12-31 15:06:22 +0530

    Create program.java

---
+++ Basics/Strings/Type of Formats/program.java
@@ -0,0 +1,19 @@
+package com.Eswar;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+
+public class Practise {
+    public static void main(String[] args) {
+        float num=1234.345432f;
+        Date date=new Date();
+        System.out.printf("Formatted String %.2f",num);
+        System.out.println();
+        System.out.printf("Pie: %.3f",Math.PI);
+        System.out.println();
+        System.out.printf("%tT",date);
+
+    }
+
+}
