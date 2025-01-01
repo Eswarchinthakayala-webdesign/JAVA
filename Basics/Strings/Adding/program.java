611ce90526a5cad97da21832c13cf418ffc4c442
Ch.Eswar <150889483+Eswarchinthakayala-webdesign@users.noreply.github.com>
2025-01-01 23:47:04 +0530

    Create program.java

---
+++ Basics/Strings/Adding/program.java
@@ -0,0 +1,20 @@
+package com.Eswar;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Functions {
+
+    public static void main(String[] args) {
+      String series="";
+        for (int i = 0; i < 26; i++) {
+            char ch= (char)('A'+i);
+            series+=ch;
+
+
+        }
+        System.out.println(series);
+    }
+
+
+}
