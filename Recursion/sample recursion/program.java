7a2923a567dcbe98da7e622828e19457d20f9c97
Ch.Eswar <150889483+Eswarchinthakayala-webdesign@users.noreply.github.com>
2025-01-06 00:43:47 +0530

    Create program.java

---
+++ Recursion/sample recursion/program.java
@@ -0,0 +1,30 @@
+
+package com.Eswar;
+
+import java.util.Arrays;
+import java.util.Set;
+
+public class Functions {
+
+    public static void main(String[] args) {
+        // we need to print hello world calling each func one time
+        message();
+    }
+
+    static void message() {
+        System.out.println("Hello World");
+        message1();
+    }
+    static void message1() {
+        System.out.println("Hello World");
+        message2();
+    }  static void message2() {
+        System.out.println("Hello World");
+        message3();
+    }  static void message3() {
+        System.out.println("Hello World");
+    }
+
+
+
+}
