896cfffce85daa9d9c22f6c7875c9824d1f7dded
Ch.Eswar <150889483+Eswarchinthakayala-webdesign@users.noreply.github.com>
2025-01-06 23:36:45 +0530

    Create program.java

---
+++ Recursion/Binary Search/program.java
@@ -0,0 +1,36 @@
+package com.Eswar;
+
+public class Recursion {
+
+    public static void main(String[] args) {
+       int [] nums={1,2,3,4,5,6,7,8};
+       int target=5;
+       int index=binarySearch(nums,target,0,nums.length-1);
+        System.out.println(index);
+    }
+
+    static int binarySearch(int[] nums, int target, int start, int end) {
+
+        if(start>end)
+        {
+            return -1;
+        }
+        int mid=start+(end-start)/2;
+        if(nums[mid]==target)
+        {
+            return mid;
+        }
+        if(nums[mid]<target)
+        {
+            start=mid+1;
+        }
+        else
+        {
+            end=mid-1;
+        }
+
+        return binarySearch(nums,target,start,end);
+    }
+
+
+}
