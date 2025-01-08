21390441dd67d84bcc4d5ed79cfa93976f55be2f
Ch.Eswar <150889483+Eswarchinthakayala-webdesign@users.noreply.github.com>
2025-01-09 00:15:40 +0530

    Create program.java

---
+++ BitWise Operators/Power of two or not/program.java
@@ -0,0 +1,15 @@
+package com.Eswar;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class BitWise {
+    public static void main(String[] args) {
+      // power of two or not
+
+        int n=16;
+        System.out.println((n&(n-1))==0);
+
+    }
+}
