221ec4e2174d9c1c5ba974fa0b5e3319d541ca36
Ch.Eswar <150889483+Eswarchinthakayala-webdesign@users.noreply.github.com>
2025-01-21 18:15:58 +0530

    Create Program.java

---
+++ MATH/GCD/Program.java
@@ -0,0 +1,20 @@
+package com.Eswar;
+import java.util.*;
+
+public class LeetCode {
+
+    public static void main(String[] args) {
+        int a=105;
+        int b=224;
+        int gcd=getGcd(a,b);
+        System.out.println(gcd);
+    }
+
+   static int getGcd(int a, int b) {
+        if(a==0)
+        {
+            return b;
+        }
+        return getGcd(b%a,a);
+    }
+}
