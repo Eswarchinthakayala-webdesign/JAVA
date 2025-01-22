abf5d061f29fae198c86d6726c4d4b817e2234f4
Ch.Eswar <150889483+Eswarchinthakayala-webdesign@users.noreply.github.com>
2025-01-22 17:06:38 +0530

    Create program.java

---
+++ Recursion-Level-1/Nto1/program.java
@@ -0,0 +1,17 @@
+package com.Eswar;
+
+public class Recursion {
+
+    public static void main(String[] args) {
+        fun(5);
+    }
+    static void fun(int n)
+    {
+        if(n==0)
+        {
+            return;
+        }
+        System.out.println(n);
+        fun(n-1);
+    }
+}
