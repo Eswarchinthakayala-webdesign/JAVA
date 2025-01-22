13f4a5ae5e9437dace377cd0b8ef8b189f4b0276
Ch.Eswar <150889483+Eswarchinthakayala-webdesign@users.noreply.github.com>
2025-01-22 17:38:02 +0530

    Create program.java

---
+++ Recursion-Level-1/Sum/program.java
@@ -0,0 +1,18 @@
+package com.Eswar;
+
+public class Recursion {
+
+    public static void main(String[] args) {
+        System.out.println(sum(10));
+    }
+
+    static int sum(int n) {
+        if(n==1)
+        {
+            return n;
+        }
+       
+        return n+sum(n-1);
+
+    }
+}
