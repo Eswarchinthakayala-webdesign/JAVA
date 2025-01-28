f22afa4f54417ea70f1f5322c0e7ffdd3c74f040
Ch.Eswar <150889483+Eswarchinthakayala-webdesign@users.noreply.github.com>
2025-01-28 17:04:58 +0530

    Create program.java

---
+++ Subsets/SubsequencesWithAscii/program.java
@@ -0,0 +1,32 @@
+package com.Eswar;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+
+public class Practise {
+    public static void main(String[] args) {
+        String up="abc";
+        ArrayList<String> ans=subsetsPracII(up,"");
+        System.out.println(ans);
+
+
+     }
+
+  static ArrayList<String> subsetsPracII(String up, String p) {
+        if(up.isEmpty())
+        {
+            ArrayList<String> res=new ArrayList<>();
+            res.add(p);
+            return res;
+        }
+
+        char ch=up.charAt(0);
+
+        ArrayList<String> right=subsetsPracII(up.substring(1),p);
+        ArrayList<String> left=subsetsPracII(up.substring(1),p+ch);
+        ArrayList<String> tupas=subsetsPracII(up.substring(1),p+(ch+0));
+        right.addAll(left);
+        tupas.addAll(right);
+        return tupas;
+    }
