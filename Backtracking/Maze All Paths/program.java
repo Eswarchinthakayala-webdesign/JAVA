8f5716fab6e079d7d094683995aa53746222d701
Ch.Eswar <150889483+Eswarchinthakayala-webdesign@users.noreply.github.com>
2025-01-31 21:07:32 +0530

    Create program.java

---
+++ Backtracking/Maze All Paths/program.java
@@ -0,0 +1,47 @@
+package com.Eswar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Backtrack {
+    public static void main(String[] args) {
+
+     boolean[][] maze=new boolean[3][3];
+     AllPaths("",maze,0,0);
+
+
+    }
+
+   static void AllPaths(String p, boolean[][] maze, int r, int c) {
+        if(r==maze.length-1 && c==maze[0].length-1)
+        {
+            System.out.println(p);
+            return;
+        }
+
+        if(maze[r][c])
+        {
+            return;
+        }
+        if(r<maze.length-1)
+        {
+            maze[r][c]=true;
+            AllPaths(p+'D',maze,r+1,c);
+        }
+        if(c<maze[0].length-1)
+        {
+            maze[r][c]=true;
+            AllPaths(p+'R',maze,r,c+1);
+        }
+        if(r>0)
+        {
+            maze[r][c]=true;
+            AllPaths(p+'U',maze,r-1,c);
+        }
+        if(c>0)
+        {
+            maze[r][c]=true;
+            AllPaths(p+'L',maze,r,c-1);
+        }
+
+    }
