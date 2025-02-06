efb61707055a36024d7a2b8e95e36987a1271f86
Ch.Eswar <150889483+Eswarchinthakayala-webdesign@users.noreply.github.com>
2025-02-06 18:16:13 +0530

    Create program.java

---
+++ OOPS-1/Assigning Values/program.java
@@ -0,0 +1,25 @@
+package com.Eswar;
+
+public class OOPS {
+    public static void main(String[] args) {
+    // to create a class and access properties in it
+
+
+        Student student1=new Student();
+        student1.rNo=40;
+        student1.name="Eswar";
+        student1.marks=22.3f;
+        System.out.println(student1.rNo); //0
+        System.out.println(student1.name); //null
+        System.out.println(student1.marks); //0.0
+
+    }
+
+}
+class Student
+{
+    int rNo;
+    String name;
+    float marks;
+}
+
