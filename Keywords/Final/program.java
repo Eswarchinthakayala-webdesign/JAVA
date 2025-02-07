6b2d150d8e4f82a01f3be751f269b0215a1d7de0
Ch.Eswar <150889483+Eswarchinthakayala-webdesign@users.noreply.github.com>
2025-02-07 22:32:19 +0530

    Create program.java

---
+++ Keywords/Final/program.java
@@ -0,0 +1,15 @@
+package com.Eswar;
+import java.util.*;
+public class WrapperExample {
+    public static void main(String[] args) {
+        final int INC=5;
+
+        System.out.println(INC);
+        Integer num=45;
+        Integer num1=num;
+        num=35;
+        System.out.println(num1);
+
+
+    }
+}
