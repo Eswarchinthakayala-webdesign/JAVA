32c27eef5d6eef0e1c65982bba6c9dfa2b347e4e
Ch.Eswar <150889483+Eswarchinthakayala-webdesign@users.noreply.github.com>
2025-02-09 21:37:43 +0530

    Create program.java

---
+++ OOP-2/packages/Eswar/program.java
@@ -0,0 +1,10 @@
+package com.eswar.packages;
+
+import static com.sriram.packages.Greeting.message;
+
+public class Greeting {
+    public static void main(String[] args) {
+        System.out.println("package Eswar");
+        message();
+    }
+}
