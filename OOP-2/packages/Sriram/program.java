d48dc354ff1b7fb908ab391930689748fe5b33cc
Ch.Eswar <150889483+Eswarchinthakayala-webdesign@users.noreply.github.com>
2025-02-09 21:38:00 +0530

    Create program.java

---
+++ OOP-2/packages/Sriram/program.java
@@ -0,0 +1,11 @@
+package com.sriram.packages;
+
+public class Greeting {
+    public static void main(String[] args) {
+        System.out.println("package sriram");
+    }
+    public static void message()
+    {
+        System.out.println("This is Message");
+    }
+}
