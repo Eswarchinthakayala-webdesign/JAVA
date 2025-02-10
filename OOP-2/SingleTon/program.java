5f2ac1b4741bf6136224ddf727e01a86d02030c1
Ch.Eswar <150889483+Eswarchinthakayala-webdesign@users.noreply.github.com>
2025-02-10 22:58:22 +0530

    Create program.java

---
+++ OOP-2/SingleTon/program.java
@@ -0,0 +1,16 @@
+package com.sriram.staticExample;
+
+public class SingleTon {
+
+    private SingleTon(){}
+
+    private static SingleTon instance;
+    public static  SingleTon getInstance()
+    {
+        if(instance==null)
+        {
+            instance=new SingleTon();
+        }
+        return instance;
+    }
+}
