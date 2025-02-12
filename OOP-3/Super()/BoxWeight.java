cdd9e6d3673c9a0de6555904ec326787ef35f7a0
Ch.Eswar <150889483+Eswarchinthakayala-webdesign@users.noreply.github.com>
2025-02-12 21:29:56 +0530

    Create BoxWeight.java

---
+++ OOP-3/Super()/BoxWeight.java
@@ -0,0 +1,21 @@
+package com.sriram.OOP3;
+
+public class BoxWeight extends Box {
+    double weight;
+    public BoxWeight()
+    {
+
+        this.weight=-1;
+    }
+    BoxWeight(BoxWeight other)
+    {
+        super(other);
+       this.weight=other.weight;
+    }
+    public BoxWeight(double l,double w,double h,double weight)
+    {
+        super(l,w,h);
+        this.weight=weight;
+    }
+
+}
