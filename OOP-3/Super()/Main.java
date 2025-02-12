f93e4d102aeadca2cb4350efd7a6642548c8c9b8
Ch.Eswar <150889483+Eswarchinthakayala-webdesign@users.noreply.github.com>
2025-02-12 21:30:20 +0530

    Create Main.java

---
+++ OOP-3/Super()/Main.java
@@ -0,0 +1,14 @@
+package com.sriram.OOP3;
+
+public class Main {
+    public static void main(String[] args) {
+//        Box b1=new Box();
+//        System.out.println(b1.l+" "+b1.w+" "+b1.h);
+//        BoxWeight box3=new BoxWeight();
+//        System.out.println(box3.weight);
+//        System.out.println(box3.w);
+//        System.out.println(box3.h);
+//        System.out.println(box3.l);
+
+    }
+}
